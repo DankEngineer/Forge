e07f309556aec5b1643035b2d45b4ac4f6be74f3
Ethan <131631861+DankEngineer@users.noreply.github.com>
2025-02-07 19:06:18 -0500

    Create remoteshutoffread.ino
    remote shutoff stuf

---
+++ Test/remoteshutoffread/remoteshutoffread.ino
@@ -0,0 +1,28 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <Adafruit_ADXL375.h>
+#include <Adafruit_BMP085.h>
+
+Adafruit_BMP085 bmp;
+Adafruit_BNO055 bno = Adafruit_BNO055(-1, BNO055_ADDRESS_A);
+Adafruit_ADXL375 adxl = Adafruit_ADXL375(12345);
+
+void setup()
+{ 
+  Serial.begin(9600);
+  pinMode(A2, INPUT);
+}
+
+void loop(void)
+{
+  if(1500<pulseIn(A2,HIGH))
+  {
+    SerialUSB.println("Off");
+  }
+  else
+  {
+    SerialUSB.println("On");
+  }
+}
