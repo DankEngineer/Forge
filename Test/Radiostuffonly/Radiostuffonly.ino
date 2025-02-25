c99674aaf6c0ceb493fa24db50a4162d11f6e0d9
Ethan <131631861+DankEngineer@users.noreply.github.com>
2025-02-25 14:08:49 -0500

    added radio testing program
    yes

---
+++ Test/Radiostuffonly/Radiostuffonly.ino
@@ -0,0 +1,208 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <math.h>
+#include <stdio.h>
+#include <avr/dtostrf.h>
+#include <MemoryFree.h>;
+#include <ZeroAPRS.h>                       //https://github.com/hakkican/ZeroAPRS
+#include <SparkFun_Ublox_Arduino_Library.h> //https://github.com/sparkfun/SparkFun_Ublox_Arduino_Library
+#include <Adafruit_BMP085.h>                //https://github.com/adafruit/Adafruit-BMP085-Library
+
+#define BattPin       A5
+#define GpsPwr        7
+#define PwDwPin       A3
+#define PowerHL       A4
+#define PttPin        3
+
+//macros
+#define GpsON       digitalWrite(GpsPwr, LOW)
+#define GpsOFF      digitalWrite(GpsPwr, HIGH)
+#define PttON       digitalWrite(PttPin, HIGH)
+#define PttOFF      digitalWrite(PttPin, LOW)
+#define RadioON     digitalWrite(PwDwPin, HIGH)
+#define RadioOFF    digitalWrite(PwDwPin, LOW)
+#define RfHiPwr     digitalWrite(PowerHL, HIGH)
+#define RfLowPwr    digitalWrite(PowerHL, LOW)
+
+#define DEVMODE // Development mode. Uncomment to enable for debugging.
+
+//******************************  APRS CONFIG **********************************
+char    CallSign[7]="KR4AIC"; //DO NOT FORGET TO CHANGE YOUR CALLSIGN
+int8_t  CallNumber=9;//SSID http://www.aprs.org/aprs11/SSIDs.txt
+char    Symbol='>'; // 'O' for balloon, '>' for car, for more info : http://www.aprs.org/symbols/symbols-new.txt
+bool    alternateSymbolTable = false ; //false = '/' , true = '\'
+
+char Frequency[9]="144.3900"; //default frequency. 144.3900 for US, 144.8000 for Europe
+
+char    comment[50] = "LightAPRS 2.0"; // Max 50 char but shorter is better.
+char    StatusMessage[50] = "LightAPRS 2.0 by TA2NHP & TA2MUN";
+//*****************************************************************************
+
+uint16_t  BeaconWait=50;  //seconds sleep for next beacon (HF or VHF). This is optimized value, do not change this if possible.
+uint16_t  BattWait=60;    //seconds sleep if super capacitors/batteries are below BattMin (important if power source is solar panel) 
+float     BattMin=3.3;    // min Volts to wake up.
+float     DraHighVolt=5.0;    // min Volts for radio module (DRA818V) to transmit (TX) 1 Watt, below this transmit 0.5 Watt.
+
+//******************************  APRS SETTINGS *********************************
+
+//do not change WIDE path settings below if you don't know what you are doing :) 
+uint8_t   Wide1=1; // 1 for WIDE1-1 path
+uint8_t   Wide2=1; // 1 for WIDE2-1 path
+
+/**
+Airborne stations above a few thousand feet should ideally use NO path at all, or at the maximum just WIDE2-1 alone.  
+Due to their extended transmit range due to elevation, multiple digipeater hops are not required by airborne stations.  
+Multi-hop paths just add needless congestion on the shared APRS channel in areas hundreds of miles away from the aircraft's own location.  
+NEVER use WIDE1-1 in an airborne path, since this can potentially trigger hundreds of home stations simultaneously over a radius of 150-200 miles. 
+ */
+uint8_t pathSize=2; // 2 for WIDE1-N,WIDE2-N ; 1 for WIDE2-N
+boolean autoPathSizeHighAlt = false; //force path to WIDE2-N only for high altitude (airborne) beaconing (over 1.000 meters (3.280 feet)) 
+boolean  aliveStatus = true; //for tx status message on first wake-up just once.
+boolean radioSetup = false; //do not change this, temp value
+static char telemetry_buff[100];// telemetry buffer
+uint16_t TxCount = 1; //increased +1 after every APRS transmission
+
+
+//******************************  GPS SETTINGS   *********************************
+int16_t   GpsResetTime=1800; // timeout for reset if GPS is not fixed
+boolean ublox_high_alt_mode_enabled = false; //do not change this
+int16_t GpsInvalidTime=0; //do not change this
+boolean gpsSetup=false; //do not change this.
+
+//********************************************************************************
+
+
+
+
+SFE_UBLOX_GPS myGPS;
+Adafruit_BMP085 bmp;
+
+void setup() 
+{
+  // While the energy rises slowly with the solar panel, 
+  // using the analog reference low solves the analog measurement errors.
+  analogReference(AR_INTERNAL1V65);
+  pinMode(PttPin, OUTPUT);
+  pinMode(GpsPwr, OUTPUT);
+  pinMode(BattPin, INPUT);
+  pinMode(PwDwPin, OUTPUT);
+  pinMode(PowerHL, OUTPUT);
+  
+  GpsOFF;
+  PttOFF;
+  RadioOFF; 
+  RfLowPwr;
+
+  SerialUSB.begin(115200);
+  // Wait up to 5 seconds for serial to be opened, to allow catching
+  // startup messages on native USB boards (that do not reset when
+  // serial is opened).
+  //Watchdog.reset();  
+  unsigned long start = millis();
+  while (millis() - start < 5000 && !SerialUSB){;}
+  //Watchdog.reset(); 
+
+  SerialUSB.println(F("Starting"));
+  Serial1.begin(9600);// for DorjiDRA818V
+
+  APRS_init();
+  APRS_setCallsign(CallSign, CallNumber);
+  APRS_setDestination("APLIGA", 0);
+  APRS_setPath1("WIDE1", Wide1);
+  APRS_setPath2("WIDE2", Wide2);
+  APRS_setPathSize(2);
+  APRS_useAlternateSymbolTable(alternateSymbolTable);
+  APRS_setSymbol(Symbol);
+  APRS_setPathSize(pathSize);
+  APRS_setGain(2);
+
+  configDra818(Frequency);
+
+  Wire.begin();
+  bmp.begin();
+
+  SerialUSB.println(F(""));
+  SerialUSB.print(F("APRS (VHF) CallSign: "));
+  SerialUSB.print(CallSign);
+  SerialUSB.print(F("-"));
+  SerialUSB.println(CallNumber);
+
+
+}
+
+void sleepSeconds(int sec) //sleep in secconds
+{
+  PttOFF;
+  RadioOFF;
+  SerialUSB.flush();
+  delay(1000*sec);
+}
+
+
+
+byte configDra818(char *freq)//radio config
+{
+  RadioON;
+  char ack[3];
+  int n;
+  delay(2000);
+  char cmd[50];//"AT+DMOSETGROUP=0,144.8000,144.8000,0000,4,0000"
+  sprintf(cmd, "AT+DMOSETGROUP=0,%s,%s,0000,4,0000", freq, freq);
+  Serial1.println(cmd);
+  SerialUSB.println("RF Config");
+  ack[2] = 0;
+  while (ack[2] != 0xa)
+  {
+    if (Serial1.available() > 0) 
+    {
+      ack[0] = ack[1];
+      ack[1] = ack[2];
+      ack[2] = Serial1.read();
+    }
+  }
+  delay(2000);
+  RadioOFF;
+
+  if (ack[0] == 0x30) 
+  {
+      SerialUSB.print(F("Frequency updated: "));
+      SerialUSB.print(freq);
+      SerialUSB.println(F("MHz"));
+  } 
+  else 
+    {
+      SerialUSB.println(F("Frequency update error!!!"));    
+    }
+  return (ack[0] == 0x30) ? 1 : 0;
+}
+
+
+
+
+void sendStatus() //send statusmessage char array
+{
+  SerialUSB.println(F("Status sending..."));
+  RfHiPwr; //DRA Power 1 Watt
+  //RfLowPwr; //DRA Power 0.5 Watt
+  RadioON;
+  delay(2000);
+  PttON;
+  delay(1000);
+  APRS_sendStatus(StatusMessage);
+  delay(10);
+  PttOFF;
+  RadioOFF;
+  delay(1000);
+  SerialUSB.print(F("Status sent - "));
+  SerialUSB.println(TxCount);
+  TxCount++;
+}
+
+
+
+void loop()  //main loop
+{
+  sendStatus();
+  sleepSeconds(BattWait);
+}
+
