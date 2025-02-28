588e665da58fc0807cb2c8b13754482931f4f2bb
Ethan <131631861+DankEngineer@users.noreply.github.com>
2025-02-28 14:20:42 -0500

    made bno085 work
    finally

--- Test/Testing_2.0/Testing_2.0.ino
+++ Test/Testing_2.0/Testing_2.0.ino
@@ -4,15 +4,18 @@
 #include <utility/imumaths.h>
 #include <Adafruit_ADXL375.h>
 #include <Adafruit_BMP085.h>
+#include "SparkFun_BNO08x_Arduino_Library.h";
 
 Adafruit_BMP085 bmp;
 Adafruit_BNO055 bno = Adafruit_BNO055(-1, BNO055_ADDRESS_A);
 Adafruit_ADXL375 adxl = Adafruit_ADXL375(12345);
+BNO08x mBNO085;
 
 void setup() 
 {
   
   Serial.begin(9600);
+  pinMode(A2, INPUT);
 
   if (!bmp.begin()) 
   {
@@ -20,11 +23,7 @@ void setup()
     while (1);
   }
 
-  if(!bno.begin()) 
-  {
-    SerialUSB.print("Ooops, BNO055(A) not detected");
-    while(1);
-  }
+ 
 
   if(!adxl.begin()) 
   {
@@ -32,26 +31,60 @@ void setup()
     while(1);
   }
 
+  if (!mBNO085.begin(0x4A, Wire, A4, A5)) 
+  {
+    SerialUSB.print("BNO085 not detected at default I2C address.");
+    while(1);
+  }
+  setReports();
+}
+
+void setReports() 
+{
+  if (mBNO085.enableGeomagneticRotationVector() == true) 
+  {
+    Serial.println("Geomagnetic Rotation vector enabled");
+  } 
+  else 
+  {
+    Serial.println("Failed to enable geomagnetic rotation vector");
+  }
 }
 
 void loop(void)
 {
+  SerialUSB.println("-------------------------------------------------------------");
+ /* if(1500<pulseIn(A2,HIGH))
+   {
+     SerialUSB.println("Remote Shutdown Status: Activated");
+   }
+   else
+   {
+     SerialUSB.println("Remote Shutdown Status: Inactive");
+   }
+  */
   //get and print orientation
-  imu::Quaternion quat = bno.getQuat();
+  if (mBNO085.getSensorEvent() == true) 
+  {
+    // Check if we got geomagnetic rotation vector data
+    if (mBNO085.getSensorEventID() == SENSOR_REPORTID_GEOMAGNETIC_ROTATION_VECTOR) 
+    {
+      SerialUSB.print("BNO quat|W: ");
+      SerialUSB.print(mBNO085.getRoll());
+
+      SerialUSB.print("  BNO quat|X: ");
+     SerialUSB.print(mBNO085.getQuatI());
+
+     SerialUSB.print("  BNO quat|Y: ");
+     SerialUSB.print(mBNO085.getQuatJ());
+
+     SerialUSB.print("  BNO quat|Z: ");
+     SerialUSB.print(mBNO085.getQuatK());
+
+     SerialUSB.println("");
+    }
+  }
   
-  SerialUSB.print("  BNO quat|W: ");
-  SerialUSB.print(quat.w());
-
-  SerialUSB.print("  BNO quat|X: ");
-  SerialUSB.print(quat.x());
-
-  SerialUSB.print("  BNO quat|Y: ");
-  SerialUSB.print(quat.y());
-
-  SerialUSB.print("  BNO quat|Z: ");
-  SerialUSB.print(quat.z());
-
-  SerialUSB.println("");
 
   // Get altitude from BMP180
   float altitude = bmp.readAltitude()* 3.28084;
@@ -64,19 +97,27 @@ void loop(void)
   adxl.getEvent(&event);
 
   /* Display the results (acceleration is measured in m/s^2) */
-   SerialUSB.print("ADXL|G: ");
-   SerialUSB.print(pow(pow(event.acceleration.x,2) + pow(event.acceleration.y,2) + pow(event.acceleration.z,2),0.5));
+   SerialUSB.print("ADXL|X: ");
+   SerialUSB.print(event.acceleration.x);
    SerialUSB.println(" m/s^2 ");
+   SerialUSB.print("ADXL|Y: ");
+   SerialUSB.print(event.acceleration.y);
+   SerialUSB.println(" m/s^2 ");
+   SerialUSB.print("ADXL|Z: ");
+   SerialUSB.print(event.acceleration.z);
+   SerialUSB.println(" m/s^2 ");
+
+   
   
-  //get and print temperature
-  int sensorValue = analogRead(A1);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V)
-  float voltage = sensorValue * (3.3/1024.0);
-  SerialUSB.print((voltage - 0.5)*100);
-  SerialUSB.println(" Degreese C");
+   //get and print temperature
+   int sensorValue = analogRead(A1);
+   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V)
+   float voltage = sensorValue * (3.3/1024.0);
+   SerialUSB.print((voltage - 0.5)*100);
+   SerialUSB.println(" Degrees C");
 
 
-  delay(10);
+  delay(100);
 
 
 }
